d60910bc4f09c0e37ad8c34a1a39a10737052676
Your Name <www.mdabirahmedsl7162@gmail.com>
2023-11-27 21:53:41 +0600

    my newslatter section done

--- src/Firebase/ErrorPage.jsx
+++ src/Firebase/ErrorPage.jsx
@@ -2,7 +2,7 @@ import error from '../assets/error.jpg'
 
 const ErrorPage = () => {
     return (
-        <div className="mt-32">
+        <div className="mt-31">
         <img className='w-9/12 mx-auto' src={error} alt="" />
         </div>
     );
